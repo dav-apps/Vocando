951a3456df95996e70475c43c38ddb271ea98a08
David <zurek.dav@gmail.com>
2025-05-23 22:25:59 +0200

    Add basic text with dictionary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,32 @@ import * as DavUIComponents from "dav-ui-components"
 	styleUrl: "./app.component.scss"
 })
 export class AppComponent {
+	dictionary: { [key: string]: { description: string } } = {
+		Anna: { description: "Anna ist ein Mädchen, das in Deutschland lebt." },
+		Österreich: { description: "Österreich ist ein Land in Europa." },
+		Deutschland: { description: "Deutschland ist ein Land in Europa." },
+		München: { description: "München ist eine Stadt in Deutschland." },
+		Klara: { description: "Klara ist Annas Schwester." }
+	}
+	text = `
+Mein Name ist Anna. Ich komme aus Österreich und lebe seit drei Jahren in Deutschland. Ich bin 15 Jahre alt und habe zwei Geschwister: Meine Schwester heißt Klara und ist 13 Jahre alt, mein Bruder Michael ist 18 Jahre alt. Wir wohnen mit unseren Eltern in einem Haus in der Nähe von München. Meine Mutter ist Köchin, mein Vater arbeitet in einer Bank.
+
+Ich lese gerne und mag Tiere: Wir haben einen Hund, zwei Katzen und im Garten einen Teich mit Goldfischen. Ich gehe auch gerne in die Schule, mein Lieblingsfach ist Mathematik. Physik und Chemie mag ich nicht so gerne.
+
+Nach der Schule gehe ich oft mit meinen Freundinnen im Park spazieren, manchmal essen wir ein Eis. Am Samstag gehen wir oft ins Kino. Am Sonntag schlafe ich lange, dann koche ich mit meiner Mutter das Mittagessen. Nach dem Essen gehen wir mit dem Hund am See spazieren. Sonntag ist mein Lieblingstag!
+	`
+	preparedText = ""
+
 	constructor() {
 		DavUIComponents.setLocale("de-DE")
+	}
+
+	ngOnInit() {
+		for (let word of Object.keys(this.dictionary)) {
+			this.text = this.text.replace(
+				new RegExp(`\\b${word}\\b`, "g"),
+				`<span class="highlight">${word}</span>`
+			)
+		}
 	}
 }
