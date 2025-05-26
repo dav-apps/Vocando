efd2278356ef8aa8b2fd6b94c40691621f569b29
David <zurek.dav@gmail.com>
2025-05-26 22:41:50 +0200

    Add bottom navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core"
 import { NavigationStart, Router } from "@angular/router"
 import {
 	faCircleUser as faCircleUserSolid,
-	faGear as faGearSolid
+	faGear as faGearSolid,
+	faHouse as faHouseSolid
 } from "@fortawesome/free-solid-svg-icons"
+import { faSparkles as faSparklesSolid } from "@fortawesome/pro-solid-svg-icons"
 import {
 	faCircleUser as faCircleUserRegular,
-	faGear as faGearRegular
+	faGear as faGearRegular,
+	faHouse as faHouseRegular,
+	faSparkles as faSparklesRegular
 } from "@fortawesome/pro-regular-svg-icons"
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
 import * as DavUIComponents from "dav-ui-components"
@@ -26,6 +30,10 @@ export class AppComponent {
 	faCircleUserRegular = faCircleUserRegular
 	faGearSolid = faGearSolid
 	faGearRegular = faGearRegular
+	faHouseSolid = faHouseSolid
+	faHouseRegular = faHouseRegular
+	faSparklesSolid = faSparklesSolid
+	faSparklesRegular = faSparklesRegular
 	dictionary: { [key: string]: { description: string } } = {
 		Anna: { description: "Anna ist ein Mädchen, das in Deutschland lebt." },
 		Österreich: { description: "Österreich ist ein Land in Europa." },
