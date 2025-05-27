528f58952827eb226598f7acb26f37586e3c0e3e
David <zurek.dav@gmail.com>
2025-05-27 22:09:33 +0200

    Add empty StartPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router"
-import { ArticlePageComponent } from "./pages/article-page/article-page.component"
+import { StartPageComponent } from "./pages/start-page/start-page.component"
 import { DiscoverPageComponent } from "./pages/discover-page/discover-page.component"
+import { ArticlePageComponent } from "./pages/article-page/article-page.component"
 
 export const routes: Routes = [
+	{
+		path: "",
+		component: StartPageComponent
+	},
 	{
 		path: "discover",
 		component: DiscoverPageComponent
---
+++ src/app/pages/start-page/start-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core"
+
+@Component({
+	templateUrl: "./start-page.component.html",
+	styleUrls: ["./start-page.component.scss"],
+	standalone: true
+})
+export class StartPageComponent {}
